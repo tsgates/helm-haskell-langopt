946ac59bf377a948c96ea38dc0a19de243887ed1
Taesoo Kim <taesoo@mit.edu>
2012-12-22 14:22:28 -0800

    cleanup

--- langopt.hs
+++ langopt.hs
@@ -10,22 +10,19 @@ import Text.Regex
   
 urlGHC = "http://www.haskell.org/ghc/docs/latest/html/users_guide/flag-reference.html"
 
-asText = arr $ \tree -> mconcat $ map N.getText $ N.nodesTree tree
+asText = arr $ \tree -> mconcat $ map N.getText (N.nodesTree tree)
   where mconcat = concatMap (maybe "" id)
 
-trim = map $ \(o, d) -> (drop 2 o, clean d)
-  where clean d = subRegex (mkRegex "[ \n\"]+") d " "
+trim = map $ \(o, d) -> (drop 2 o, cleanup d)
+  where cleanup d = subRegex (mkRegex "[ \n\"]+") d " "
 
 onlyLang = filter $ \(o, _) -> (isPrefixOf "-X" o)
 
 main = do
     doc <- fromUrl urlGHC
     opt <- runX $ doc >>> css "div"
-                      >>> hasAttrValue "title"
-                          (isInfixOf "Language options")
+                      >>> hasAttrValue "title" (isInfixOf "Language options")
                       //> (getXPathTrees "tr/td[1]" &&& getXPathTrees "tr/td[2]")
                       >>> (asText *** asText)
-    
     forM_ (trim $ onlyLang opt) $ \(o, d)->
       hPrintf stdout "\"%-30s: %s\"\n" o d
-    return ()
